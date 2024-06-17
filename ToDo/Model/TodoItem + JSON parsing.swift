ae5e04bec7e7c922fcdc9016c702aef8197b1959
Alexander <oBelyash@yandex.ru>
2024-06-17 23:29:13 +0300

    TodoItem created

---
+++ ToDo/Model/TodoItem + JSON parsing.swift
@@ -0,0 +1,16 @@
+//
+//  TodoItem + JSON parsing.swift
+//  ToDo
+//
+//  Created by Александр Андреев on 17.06.2024.
+//
+
+import Foundation
+extension TodoItem {
+    
+    static func parse(json: Any) -> TodoItem? {
+        
+        
+        return nil
+    }
+}
