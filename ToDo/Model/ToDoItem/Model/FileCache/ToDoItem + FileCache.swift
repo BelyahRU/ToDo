8a73bff8524dafb3e4eca43763259072d57dd595
Alexander <oBelyash@yandex.ru>
2024-07-10 17:34:20 +0300

    created some folders

---
+++ ToDo/Model/ToDoItem/Model/FileCache/ToDoItem + FileCache.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoItem + FileCache.swift
+//  ToDo
+//
+//  Created by Александр Андреев on 10.07.2024.
+//
+
+import Foundation
+extension ToDoItemModel {
+        
+    //сохранение в файл
+    func saveInFile(fileName: String) {
+        fileCache.saveInFile(items: self.toDos, fileName: fileName)
+        
+    }
+    //загрузка данных из файла
+    func uploadFromFile(fileName: String) -> [TodoItem] {
+        fileCache.uploadFromFile(fileName: fileName)
+    }
+
+}
